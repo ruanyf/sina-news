a947c2eaec3dd86d6795c5c31937ef7b6793b513
ruanyf <yifeng.ruan@gmail.com>
2020-12-14 12:58:03 +0000

    Deploying to gh-pages from  @ 825467138ff68995b5b20f8d47680ca13d5f1527 🚀

---
+++ page.js
@@ -0,0 +1,18 @@
+fetch('./rss.json')
+.then(async function (response) {
+  const res = await response.json();
+  const items = res.items;
+
+  const list = document.querySelector('.list');
+
+  items.forEach(i => {
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    const timeObj = new Date(i.date_modified);
+    p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${timeObj.getHours()}:${timeObj.getMinutes()}</a>）`;
+    li.appendChild(p);
+    list.appendChild(li);
+  });
+})
+
+
