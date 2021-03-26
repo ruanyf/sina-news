6468a1774edaab346bc7266408e83652bf85c9a3
tienouc <willtien@outlook.com>
2021-03-26 16:06:27 +0800

    optimize time display

--- template/page.js
+++ template/page.js
@@ -5,12 +5,17 @@ fetch('./rss.json')
 
   const list = document.querySelector('.list');
   const fragment = document.createDocumentFragment();
-
+  const checkTime = function (time) {
+    if (time < 10) {
+      time = '0' + time;
+    }
+    return time;
+  }
   items.forEach(i => {
     const li = document.createElement('li');
     const p = document.createElement('p');
     const timeObj = new Date(i.date_modified);
-    p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${timeObj.getHours()}:${timeObj.getMinutes()}</a>）`;
+    p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${checkTime(timeObj.getHours())}:${checkTime(timeObj.getMinutes())}</a>）`;
     li.appendChild(p);
     fragment.appendChild(li);
   });
